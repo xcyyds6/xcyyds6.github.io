0aa8f8d5df2dd071ec46449fee741658e09ce5e6
星辰 <2026093925@qq.com>
2022-07-24 01:13:21 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    plugins:{
+        '@vuepress/active-header-links':{},
+        'autobar':{}
+    },
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+      nav: [
+        { text: '百度', link: 'https://www.baidu.com', target:'_self', rel:'' },
+        { text: 'Guide', link: '/guide/', target:'_blank' },
+        {
+          text: 'Languages',
+          ariaLabel: 'Language Menu',
+          items: [
+            { text: 'Chinese', link: '/language/chinese/' },
+            { text: 'Japanese', link: '/language/japanese/' }
+          ]
+        }
+      ]
+    }
+  }
